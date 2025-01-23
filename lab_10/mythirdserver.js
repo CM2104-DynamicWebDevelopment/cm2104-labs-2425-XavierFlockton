1c02f26f5d8cbd549c94bd77f5a69c103ff842c5
XavierFlockton <x.flockton@rgu.ac.uk>
2025-01-23 11:12:53 +0000

    fix mythird server

--- lab_10/mythirdserver.js
+++ lab_10/mythirdserver.js
@@ -1,14 +1,11 @@
 
 var http = require('http');
-var currentdate = require('./mymodule');
 var knockknock = require('knock-knock-jokes');
 
 
 http.createServer(function (req, res) {
 
  res.writeHead(200, {'Content-Type': 'text/html'});
- res.write("The date and time are currently: " + currrentdate.myDateTime());
- res.end('Hello World!');
  res.end(randomJoke);
  
 }).listen(8080);
