88f9d866e4cb57fab07627198ce8d3d1aaf56d06
XavierFlockton <x.flockton@rgu.ac.uk>
2025-01-30 11:03:08 +0000

    static files

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+app.use(express.static('public'))
 app.get('/', function(req, res){
     res.send("Hello world! by express");
 });
@@ -32,5 +32,11 @@ app.get('/calc', function(req, res){
         res.send("That is not a  accepted operator");
     }
 });
+
+app.get('/getform', function(req, res){
+    var name = req.query.name;
+    var quest = req.query.quest;
+     res.send("Hi "+name+" I am sure you will "+quest) ;
+    });
 
 app.listen(8080);
