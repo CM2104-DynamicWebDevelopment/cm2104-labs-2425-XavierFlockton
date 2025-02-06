bd91631e865b525d93917352d954b0e3a8cec7b2
XavierFlockton <x.flockton@rgu.ac.uk>
2025-02-06 12:12:37 +0000

    form fix

--- lab_12/server.js
+++ lab_12/server.js
@@ -54,7 +54,7 @@ async function getTracks(searchterm, res) {
 }
 
 app.get('/search', function (req, res) {
-    var searchterm = req.query.searchterm;
+    var searchterm = req.query.search;
     getTracks(searchterm, res);
 });
 
