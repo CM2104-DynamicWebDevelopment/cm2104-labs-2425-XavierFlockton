85356b3b4340956345a195bdb2fc9cf1b6b9b8d3
XavierFlockton <x.flockton@rgu.ac.uk>
2025-02-06 11:11:55 +0000

    coded web app

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var app = express();
- app.use(express.static('public'))
- app.get('/'
-, function(req, res){
- res.send("Hello world! by express");
+app.use(express.static('public'))
+var SpotifyWebApi = require('spotify-web-api-node');
+var spotifyApi = new SpotifyWebApi({
+    clientId: 'b61389fd0bb54b54ae2bba0ff41f5889',
+    clientSecret: '67aeeeea812d4d30abfbeab096035ac6'
+})
+
+spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+
+
+        spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function (err) {
+        console.log(
+            'Something went wrong when retrieving an access token',
+            err.message 
+        );
+    }
+)
+
+
+async function getTracks(searchterm, res) {
+
+    spotifyApisearchTracks(searchterm).then(function (data) {
+        res.send(JSON.stringify(data.body));
+    }, function (err) {
+        console.error(err);
+    });
+}
+
+app.get('/searchLove', function (req, res) {
+    getTracks('love', res);
 });
 app.listen(8080);
